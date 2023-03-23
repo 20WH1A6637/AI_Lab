eb890aff1a8f5f24080daaba9a158d63efe5a206
Thopuri Harshitha <93579922+20WH1A6637@users.noreply.github.com>
2023-03-23 15:49:05 +0530

    Update NQueens.pl

--- NQueens.pl
+++ NQueens.pl
@@ -9,3 +9,26 @@ attack(Q, Qs) :- attack(Q, 1, Qs).
 attack(X, N, [Y|_]) :- X =:= Y + N.
 attack(X, N, [Y|_]) :- X =:= Y - N.
 attack(X, N, [_|Ys]) :- N1 is N + 1, attack(X, N1, Ys).
+
+
+OUTPUT
+
+
+?- pwd.
+% /home/cse/
+true.
+
+?- cd('/home/cse/Downloads/').
+true.
+
+?- pwd.
+% /home/cse/Downloads/
+true.
+
+?- [nqueens].
+true.
+
+?- nqueens(4, Q).
+Correct to: "queens(4,Q)"? 
+Please answer 'y' or 'n'? yes
+Q = [3, 1, 4, 2] 
